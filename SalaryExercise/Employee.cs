f65de3f07fdaf4d413f903b85276da856de820fc
henri <henri@HENRIQUE>
2024-04-23 23:12:56 -0300

    Add project files.

---
+++ SalaryExercise/Employee.cs
@@ -0,0 +1,19 @@
+﻿namespace SalaryExercise
+{
+    class Employee
+    {
+        public string Name = "";
+        public double GrossSalary;
+        public double Tax;
+
+        public double CalculateNetSalary()
+        {
+            return GrossSalary - Tax;
+        }
+
+        public double IncreaseSalary(double percentage)
+        {
+            return (GrossSalary * (percentage / 100.00) + GrossSalary) - Tax;
+        }
+    }
+}
---
+++ SalaryExercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace SalaryExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            Console.Write("Nome: ");
+            employee.Name = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            employee.GrossSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            employee.Tax = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Funcionário: {0}, $ {1}", employee.Name, employee.CalculateNetSalary().ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double currentTax = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Dados atualizados: {0}, $ {1}", employee.Name, employee.IncreaseSalary(currentTax).ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
